cd87a0cb593da79d46c976fc49585945643317fb
FrancisAyala123 <lara38507@gmail.com>
2023-11-03 16:34:41 -0600

    actualiazaciones 4 vistas

---
+++ app/src/main/java/com/example/appmenu/AgregarCategoriaActivity.java
@@ -0,0 +1,14 @@
+package com.example.appmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarCategoriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_categoria);
+    }
+}
