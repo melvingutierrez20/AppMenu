295528a577695911a1d80e4097310d19c912983f
FrancisAyala123 <lara38507@gmail.com>
2023-11-03 22:10:31 -0600

    actualiazaciones layout

---
+++ app/src/main/java/com/example/appmenu/AgregarPlatillosActivity.java
@@ -0,0 +1,14 @@
+package com.example.appmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarPlatillosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_platillos);
+    }
+}
