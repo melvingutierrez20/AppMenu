5a9fa069f58dd36348d530598ad989f20ac209d3
manue <carlos.rivas1@catolica.edu.sv>
2023-11-08 09:48:13 -0600

    Registro de Usuario

--- app/src/main/java/com/example/appmenu/Account.java
+++ app/src/main/java/com/example/appmenu/Account.java
@@ -1,14 +1,83 @@
 package com.example.appmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Account extends AppCompatActivity {
+
+
+    EditText emailEditText, passwordEditText, confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        emailEditText  = findViewById(R.id.email_edit_text);
+        passwordEditText  = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText  = findViewById(R.id.confirm_password_edit_text);
+        createAccountBtn = findViewById(R.id.create_account_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtnTextView = findViewById(R.id.login_text_view_btn);
+
+        createAccountBtn.setOnClickListener( v->createAccount());
+        loginBtnTextView.setOnClickListener(v-> finish());
+    }
+    void createAccount() {
+        //variables que almacenan el contenido del usuario ingresa desde la vista
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+
+        createAccountInFirebase(email,password);
+    }
+    void createAccountInFirebase(String email,String password){
+        changeInProgress(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(Account.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        changeInProgress(false);
+                        if(task.isSuccessful()){
+                            Toast.makeText(Account.this, "Cuenta creada, Revisa tu correo para verificar", Toast.LENGTH_SHORT).show();
+                            firebaseAuth.getCurrentUser().sendEmailVerification();
+                            firebaseAuth.signOut();
+                            finish();
+                        }else{
+                            Toast.makeText(Account.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }
+        );
+    }
+    void changeInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            createAccountBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            createAccountBtn.setVisibility(View.VISIBLE);
+        }
     }
 }
--- app/src/main/java/com/example/appmenu/Login.java
+++ app/src/main/java/com/example/appmenu/Login.java
@@ -2,13 +2,20 @@ package com.example.appmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
+
+    TextView createAccountBtnTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        createAccountBtnTextView = findViewById(R.id.create_account_text_view_btn);
+
+        createAccountBtnTextView.setOnClickListener((v)->startActivity(new Intent(Login.this,Account.class)));
     }
 }
