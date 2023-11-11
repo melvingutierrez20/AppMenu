0eb69de446d0f217d04677d1e4d96a81b89cc3a0
Francis Ayala <francis.ayala@catolica.edu.sv>
2023-11-10 21:47:34 -0600

    agregar foto categorias

--- app/src/main/java/com/example/appmenu/CategoriasActivity.java
+++ app/src/main/java/com/example/appmenu/CategoriasActivity.java
@@ -1,8 +1,12 @@
 package com.example.appmenu;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appmenu.R;
 
 public class CategoriasActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class CategoriasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorias);
+
+
+        RecyclerView recyclerView = findViewById(R.id.rvCategorias);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+
+
+        recyclerView.setLayoutManager(layoutManager);
+
+
     }
 }
