2c81cad51f9872602d799c1191cf9b6ad41cc8a1
rRicardo-1 <ricardo.moreno@catolica.edu.sv>
2023-11-14 00:14:10 -0600

    Mostrar categorias

---
+++ app/src/main/java/com/example/appmenu/AdaptadorCategoria.java
@@ -0,0 +1,75 @@
+package com.example.appmenu;
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
+
+public class AdaptadorCategoria extends RecyclerView.Adapter<AdaptadorCategoria.ViewHolder>{
+
+    private final List<CategoriaModel> categoriaModelList;
+    private final Context context;
+    private AdapterView.OnItemClickListener itemClickListener;
+
+    public AdaptadorCategoria(Context context, List<CategoriaModel> categoriaModelList) {
+        this.context = context;
+        this.categoriaModelList = categoriaModelList;
+
+    }
+
+    @NonNull
+    @Override
+    public AdaptadorCategoria.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_categoria, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        CategoriaModel categoriaM = categoriaModelList.get(position);
+        holder.tvNom_Categoria.setText(categoriaM.getTitle());
+
+        //cargar la imagen utilizando Glide
+        Glide.with(context)
+                .load(categoriaM.getImageUrl())
+                .placeholder(R.drawable.img_9)
+                .error(R.drawable.img_9)
+                .into(holder.iv_Categoria);
+        //
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoriaModelList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView iv_Categoria;
+        ImageView btnBorrar;
+        TextView tvNom_Categoria;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvNom_Categoria = itemView.findViewById(R.id.tvNomCategoria);
+            iv_Categoria = itemView.findViewById(R.id.ivCategoria);
+            btnBorrar = itemView.findViewById(R.id.ibtnBorrar);
+
+        }
+    }
+}
--- app/src/main/java/com/example/appmenu/CategoriasActivity.java
+++ app/src/main/java/com/example/appmenu/CategoriasActivity.java
@@ -5,29 +5,60 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.appmenu.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoriasActivity extends AppCompatActivity {
 
     FloatingActionButton addCategoriaBtn;
+    RecyclerView recyclerView;
+    GridLayoutManager layoutManager;
+    AdaptadorCategoria ac;
+    private List<CategoriaModel> categoriaModelList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorias);
 
-
-        RecyclerView recyclerView = findViewById(R.id.rvCategorias);
-
-        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
-
-
-        recyclerView.setLayoutManager(layoutManager);
+        //declaracion  de variables para pasarle los elementos de la vista y librerias
+        recyclerView = findViewById(R.id.rvCategorias);
         addCategoriaBtn = findViewById(R.id.AddCategoriaBtn);
+        categoriaModelList = new ArrayList<>();
+        ac = new AdaptadorCategoria(this, categoriaModelList);
+        //layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(ac);
         addCategoriaBtn.setOnClickListener(v->startActivity(new Intent(CategoriasActivity.this, AgregarCategoriaActivity.class)));
 
+        cargaDatos();
+    }
 
+    void cargaDatos(){
+        FirebaseFirestore.getInstance().collection("categoria")
+                .addSnapshotListener((value, error)->{
+                    if(error != null){
+                        return;
+                    }
+
+                    if(value != null){
+                        categoriaModelList.clear();
+                        for(QueryDocumentSnapshot document : value){
+                            CategoriaModel categoriaModel = document.toObject(CategoriaModel.class);
+                            categoriaModelList.add(categoriaModel);
+                        }
+                        ac.notifyDataSetChanged();
+                    }
+                });
     }
 }
