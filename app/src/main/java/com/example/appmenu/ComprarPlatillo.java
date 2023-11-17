08050c1c5389cc51340920299172d04f8c88deb8
Francis Ayala <francis.ayala@catolica.edu.sv>
2023-11-16 23:27:44 -0600

    actualizacion diseno 2

--- app/src/main/java/com/example/appmenu/ComprarPlatillo.java
+++ app/src/main/java/com/example/appmenu/ComprarPlatillo.java
@@ -9,6 +9,6 @@ public class ComprarPlatillo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_comprar_platillo);
+        setContentView(R.layout.activity_comprar_producto);
     }
 }
