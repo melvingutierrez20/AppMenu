f83deb896e1edaf257c2b4588b53289234e6b8d4
melvingutierrez20 <melvin.gutierrez@catolica.edu.sv>
2023-11-18 14:08:06 -0600

    Direcionamiento de los activitys de producto

--- app/src/main/java/com/example/appmenu/Opciones.java
+++ app/src/main/java/com/example/appmenu/Opciones.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 public class Opciones extends AppCompatActivity {
 
 
-    ImageView img_categoria, img_ordenes, img_historial;
+    ImageView img_categoria, img_ordenes, img_historial, img_productos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +18,10 @@ public class Opciones extends AppCompatActivity {
         img_categoria = findViewById(R.id.imgCategoria);
         img_ordenes = findViewById(R.id.imgOrden);
         img_historial = findViewById(R.id.imgHistorial);
+        img_productos = findViewById(R.id.imgProductos);
 
         img_categoria.setOnClickListener(v->startActivity(new Intent(Opciones.this, CategoriasActivity.class)));
-        img_ordenes.setOnClickListener(v->startActivity(new Intent(Opciones.this, ComprarPlatillo.class)));
+        img_ordenes.setOnClickListener(v->startActivity(new Intent(Opciones.this, PlatillosActivity.class)));
         img_historial.setOnClickListener(v->startActivity(new Intent(Opciones.this, Historial.class)));
     }
 }
--- app/src/main/java/com/example/appmenu/PlatillosActivity.java
+++ app/src/main/java/com/example/appmenu/PlatillosActivity.java
@@ -2,7 +2,11 @@ package com.example.appmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PlatillosActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class PlatillosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_platillos);
+
+        FloatingActionButton addProductBtn = findViewById(R.id.AddCategoriaBtn);
+
+        addProductBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Iniciar la Actividad AgregarPlatillos
+                Intent intent = new Intent(PlatillosActivity.this, AgregarPlatillosActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
