d43f6f740bf3aff85038f3f2ca7108766dab53a7
melvingutierrez20 <melvin.gutierrez@catolica.edu.sv>
2023-11-23 00:26:28 -0600

    commit de modelo de cuentas

---
+++ app/src/main/java/com/example/appmenu/CuentasModel.java
@@ -0,0 +1,50 @@
+package com.example.appmenu;
+
+public class CuentasModel {
+     private String idCompra;
+     private String codigoCuenta;
+     private String productosSeleccionados;
+     private double totalPedido;
+
+    public CuentasModel() {
+    }
+
+    public CuentasModel(String idCompra, String codigoCuenta, String productosSeleccionados, double totalPedido) {
+        this.idCompra = idCompra;
+        this.codigoCuenta = codigoCuenta;
+        this.productosSeleccionados = productosSeleccionados;
+        this.totalPedido = totalPedido;
+    }
+
+    public String getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(String idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getCodigoCuenta() {
+        return codigoCuenta;
+    }
+
+    public void setCodigoCuenta(String codigoCuenta) {
+        this.codigoCuenta = codigoCuenta;
+    }
+
+    public String getProductosSeleccionados() {
+        return productosSeleccionados;
+    }
+
+    public void setProductosSeleccionados(String productosSeleccionados) {
+        this.productosSeleccionados = productosSeleccionados;
+    }
+
+    public double getTotalPedido() {
+        return totalPedido;
+    }
+
+    public void setTotalPedido(double totalPedido) {
+        this.totalPedido = totalPedido;
+    }
+}
